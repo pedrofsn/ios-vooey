dba55961ef924d6ad7dd73385ef48431f094b623
pedrofsn <pedrokra@gmail.com>
2018-03-03 13:37:38 -0300

    Initial Commit

---
+++ ios-vooey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-vooey
+//
+//  Created by Aluno on 03/03/2018.
+//  Copyright © 2018 Instituto de Informática. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
